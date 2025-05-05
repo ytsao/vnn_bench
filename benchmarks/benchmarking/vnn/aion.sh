7bf0deb9ffef6963a942654ecf024a52173caa48
ytsao <yi-nung.tsao@uni.lu>
2025-05-05 08:42:50 +0200

    [UPDATE] experimental results.

--- benchmarks/benchmarking/vnn/aion.sh
+++ benchmarks/benchmarking/vnn/aion.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-module load lang/Python/3.8.6-GCCcore-10.2.0
+
+
+
+# module load lang/Python/3.8.6-GCCcore-10.2.0
+
+module load tools/binutils/2.40-GCCcore-13.2.0
+module load devel/CMake/3.27.6-GCCcore-13.2.0
+module load tools/GLPK/5.0-GCCcore-13.2.0
+
+export BOOST_ROOT=$HOME/boost_1_84_0_install
+export BOOST_INCLUDEDIR=$BOOST_ROOT/include
+export BOOST_LIBRARYDIR=$BOOST_ROOT/lib
+
+export LD_LIBRARY_PATH=$HOME/protobuf-3.20.3-install/lib:$LD_LIBRARY_PATH
+export PATH=$HOME/Protobuf-3.20.3-install/bin:$PATH
--- benchmarks/benchmarking/vnn/dump_BaFA.py
+++ benchmarks/benchmarking/vnn/dump_BaFA.py
@@ -17,15 +17,15 @@ if __name__ == "__main__":
   network_name = sys.argv[4]
   dataset = sys.argv[5]
   relu_transformer = sys.argv[6]
-  label = sys.argv[7]
-  data_dir = sys.argv[8]
-  num_tests = sys.argv[9]
-  epsilon = sys.argv[10]
-  patch_size = sys.argv[11]
-  num_post_cons = sys.argv[12]
+  # label = sys.argv[7]
+  data_dir = sys.argv[7]
+  num_tests = sys.argv[8]
+  epsilon = sys.argv[9]
+  patch_size = sys.argv[10]
+  # num_post_cons = sys.argv[11]
   extras = []
 
-  for i in range(13, len(sys.argv)):
+  for i in range(11, len(sys.argv)):
     arg = sys.argv[i].strip().replace(' ', '-')
     print(arg)
     if arg != "" and arg != "-s": # we use "-s" when there are "no special options to be used".
@@ -35,7 +35,8 @@ if __name__ == "__main__":
         extras[-1] = extras[-1][1:]
 
   # uid = verifier.replace('.', '-') + "_" + robustness_type + "_" + network_name + "_" + dataset + "_" + epsilon
-  uid: str = f"{verifier}_{robustness_type}_{network_name}_{dataset}_{label}_{epsilon}_{data_dir}_{num_tests}_{patch_size}_{num_post_cons}"
+  # uid: str = f"{verifier}_{robustness_type}_{network_name}_{dataset}_{label}_{epsilon}_{data_dir}_{num_tests}_{patch_size}_{num_post_cons}"
+  uid: str = f"{verifier}_{robustness_type}_{network_name}_{dataset}_{epsilon}_{data_dir}_{num_tests}_{patch_size}"
   if len(extras) > 0:
     uid += "_"
     uid += "_".join(extras)
@@ -53,12 +54,12 @@ if __name__ == "__main__":
     "network_name": network_name,
     "dataset": dataset,
     "relu_transformer": relu_transformer,
-    "label": label,
+    # "label": label,
     "data_dir": data_dir,
     "num_tests": num_tests,
     "epsilon": epsilon,
     "patch_size": patch_size,
-    "num_post_cons": num_post_cons,
+    # "num_post_cons": num_post_cons,
     "datetime": datetime.datetime.now().isoformat()
   }
 
