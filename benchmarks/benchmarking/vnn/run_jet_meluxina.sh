868e19dc3503bba0338650dd213349e59a297bbd
ytsao <tasocc456@gmail.com>
2026-05-21 17:35:39 +0200

    modify run jet meluxina

--- benchmarks/benchmarking/vnn/run_jet_meluxina.sh
+++ benchmarks/benchmarking/vnn/run_jet_meluxina.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#SBATCH --time=4:00:00
+#SBATCH --time=16:00:00
 #SBATCH --partition=gpu
 #SBATCH --nodes=2
 #SBATCH --gpus-per-node=4
@@ -38,10 +38,11 @@ fi
 
 # I. Define the campaign to run.
 VNN_VERIFIER="jet"
-VERSION="gpu" # Note that this is only for the naming of the output directory, we do not verify the actual version of the solver.
+CATEGORY="acasxu_2023"
+VERSION="gpu-$CATEGORY" # Note that this is only for the naming of the output directory, we do not verify the actual version of the solver.
 CORES=1 # The number of cores used on the node.
 MACHINE=$(basename "$1" ".sh")
-INSTANCES_PATH="$BENCHMARKS_DIR_PATH/data/sat_relu/instances.csv"
+INSTANCES_PATH="$BENCHMARKS_DIR_PATH/data/acasxu_2023/instances.csv"
 
 # II. Prepare the command lines and output directory.
 VNN_COMMAND="$VNN_WORKFLOW_PATH/../../../../turbo/build/gpu-release-local/turbo"
@@ -68,4 +69,4 @@ lshw -json > $OUTPUT_DIR/$(basename "$VNN_WORKFLOW_PATH")/hardware-"$MACHINE".js
 # III. Run the experiments in parallel.
 # The `parallel` command spawns one `srun` command per experiment, which executes the orca verifier with the right resources.
 COMMANDS_LOG="$OUTPUT_DIR/$(basename "$VNN_WORKFLOW_PATH")/jobs.log"
-parallel --verbose --no-run-if-empty --rpl '{} uq()' -k --colsep ',' -j $NUM_PARALLEL_EXPERIMENTS --resume --joblog $COMMANDS_LOG $SRUN_COMMAND $VNN_COMMAND -s -v -i -t {3} -disable_simplify -arch fbarebones -vnnlib_path ${BENCHMARKS_DIR_PATH}/data/sat_relu/{2} -onnx_path ${BENCHMARKS_DIR_PATH}/data/sat_relu/{1} '2>&1' '|' python3 $DUMP_PY_PATH $OUTPUT_DIR $VNN_VERIFIER {1} {2} {3} :::: $INSTANCES_PATH
+parallel --verbose --no-run-if-empty --rpl '{} uq()' -k --colsep ',' -j $NUM_PARALLEL_EXPERIMENTS --resume --joblog $COMMANDS_LOG $SRUN_COMMAND $VNN_COMMAND -s -v -i -t {3} -disable_simplify -arch fbarebones -vnnlib_path ${BENCHMARKS_DIR_PATH}/data/${CATEGORY}/{2} -onnx_path ${BENCHMARKS_DIR_PATH}/data/${CATEGORY}/{1} '2>&1' '|' python3 $DUMP_PY_PATH $OUTPUT_DIR $VNN_VERIFIER {1} {2} {3} :::: $INSTANCES_PATH
